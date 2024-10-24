c998fef9c88887a39faec73c3b0e5be71dbd21ad
Sai Kasyap <jv.saikasyap@gmail.com>
2024-10-24 10:38:13 -0500

    Time: 0 ms (100.00%) | Memory: 11.5 MB (64.22%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        count = 0
+        while num>0:
+            if (num)%2 == 0:
+                num = (num // 2)
+                count += 1
+            if (num)%2 == 1:
+                num = (num - 1)
+                count += 1
+        return count
+        
+        
+        
+        """
+        :type num: int
+        :rtype: int
+        """
+        
