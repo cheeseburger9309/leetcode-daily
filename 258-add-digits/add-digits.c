7035f5b6554e0068c0b533c43331329040182ebb
Sai Kasyap <jv.saikasyap@gmail.com>
2025-07-11 22:37:11 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (12.20%) - LeetSync

--- 258-add-digits/add-digits.c
+++ 258-add-digits/add-digits.c
@@ -5,8 +5,8 @@ int addDigits(int num) {
     if (num%9 == 0){
         return 9;
     }
-    while (num > 9){
-        num = num - 9;
-    } 
-    return num;
+    else{
+        return num%9;
+    }
+    
 }
