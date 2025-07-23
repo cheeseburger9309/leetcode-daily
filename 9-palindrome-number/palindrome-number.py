05e9cf5fa75f890d0c25fbad7d2d660c6e60d580
Sai Kasyap <jv.saikasyap@gmail.com>
2025-07-22 23:25:26 -0500

    Time: 58 ms (5.05%) | Memory: 12.5 MB (51.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,22 +4,31 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        array = []
-        if x<0:
-            return False
-        while x > 0:
-            units = x%10
-            array.append(units)
-            x = int(x//10)
-        if (len(array)%2) == 0:
-            n = int(len(array)/2)
-            print(array[:n], array[-1:n-1:-1])
-            return array[:n] == array[-1:n-1:-1]
-        else:
-            n = int((len(array) - 1)/2)
-            print(array[:n], array[-1:n:-1])
-            return array[:n] == array[-1:n:-1]
-        # print(array[:n], array[n+1:])
+        sum = 0
+        temp = x
+        while temp > 0:
+            units = temp%10
+            sum = (sum*10) + units
+            temp = int(temp//10)
+        print(sum, x)
+        return(sum == x)
+
+        # array = []
+        # if x<0:
+        #     return False
+        # while x > 0:
+        #     units = x%10
+        #     array.append(units)
+        #     x = int(x//10)
+        # if (len(array)%2) == 0:
+        #     n = int(len(array)/2)
+        #     print(array[:n], array[-1:n-1:-1])
+        #     return array[:n] == array[-1:n-1:-1]
+        # else:
+        #     n = int((len(array) - 1)/2)
+        #     print(array[:n], array[-1:n:-1])
+        #     return array[:n] == array[-1:n:-1]
+        # # print(array[:n], array[n+1:])
         
 
         
