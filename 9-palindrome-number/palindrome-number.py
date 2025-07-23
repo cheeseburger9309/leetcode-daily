766e1def17fe8af2fb4fd40e6cbc2368e10e4d4a
Sai Kasyap <jv.saikasyap@gmail.com>
2025-07-22 23:29:16 -0500

    Time: 17 ms (25.44%) | Memory: 12.6 MB (10.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -13,22 +13,7 @@ class Solution(object):
         # print(sum, x)
         return(sum == x)
 
-        # array = []
-        # if x<0:
-        #     return False
-        # while x > 0:
-        #     units = x%10
-        #     array.append(units)
-        #     x = int(x//10)
-        # if (len(array)%2) == 0:
-        #     n = int(len(array)/2)
-        #     print(array[:n], array[-1:n-1:-1])
-        #     return array[:n] == array[-1:n-1:-1]
-        # else:
-        #     n = int((len(array) - 1)/2)
-        #     print(array[:n], array[-1:n:-1])
-        #     return array[:n] == array[-1:n:-1]
-        # # print(array[:n], array[n+1:])
+        
         
 
         
