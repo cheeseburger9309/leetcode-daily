55747438a38462890a7179ed64423ff8834ebd3b
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2025-09-16 22:45:58 -0500

    Time: 90 ms (5.44%) | Memory: 20.2 MB (92.84%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -12,27 +12,27 @@ class Solution(object):
         # return False
 
         # Sorting approach (Efficient approach):
-        # nums.sort()
+        nums.sort()
         # if len(nums) > 1:
-        #     for i in range(len(nums) - 1):
-        #         print(nums[i])
-        #         print(nums[i+1])
-        #         if nums[i] == nums[i+1]:
-        #             return True
-        #     return False
-        # else:
-        #     return False
-
-        # Set Approach
-        nums_set = set()
-        # length = 1
-
-        for i in range(len(nums)):
-            if nums[i] in nums_set:
-                return True 
-            else:
-                nums_set.add(nums[i])
+        for i in range(1, len(nums)):
+            # print(nums[i])
+            # print(nums[i+1])
+            if nums[i] == nums[i-1]:
+                return True
         return False
+        # else:
+            # return False
+
+        # # Set Approach
+        # nums_set = set()
+        # # length = 1
+
+        # for i in range(len(nums)):
+        #     if nums[i] in nums_set:
+        #         return True 
+        #     else:
+        #         nums_set.add(nums[i])
+        # return False
 
             # if length == len(nums_set):
             #     length += 1
