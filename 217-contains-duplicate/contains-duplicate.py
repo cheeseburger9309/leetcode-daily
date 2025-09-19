f7735db5fdb623049261a34d383ed3fe63167f14
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2025-09-18 22:33:24 -0500

    Time: 24 ms (26.75%) | Memory: 26.1 MB (17.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,50 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Brute-force: 
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] == nums[j]:
-        #             return True
-        # return False
+        dupset = set()
 
-        # Sorting approach (Efficient approach):
-        nums.sort()
-        # if len(nums) > 1:
-        for i in range(1, len(nums)):
-            # print(nums[i])
-            # print(nums[i+1])
-            if nums[i] == nums[i-1]:
+        for n in nums:
+            if n in dupset:
                 return True
+            dupset.add(n)
         return False
-        # else:
-            # return False
-
-        # # Set Approach
-        # nums_set = set()
-        # # length = 1
-
-        # for i in range(len(nums)):
-        #     if nums[i] in nums_set:
-        #         return True 
-        #     else:
-        #         nums_set.add(nums[i])
-        # return False
-
-            # if length == len(nums_set):
-            #     length += 1
-            # else:
-            #     return False
-        
-        
-            
-
-
-
-
-
-
-
 
         
-
+        
