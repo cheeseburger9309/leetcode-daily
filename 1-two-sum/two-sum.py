cef40e17430267be921f9321d009de450bbffd9f
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2025-09-19 00:13:47 -0500

    Time: 2027 ms (38.98%) | Memory: 13.4 MB (15.94%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,31 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashmap = {}
-        # new.sort()
-        
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
-
-            # for j in range(i+1, len(nums)):
-            #     if nums[j] == target - nums[i]:
-            #         return[i, j]
-                
-
-        # for i in range(len(nums)):
-        #     t = target - nums[i]
-        #     new[nums[i]] = t
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
         
-        # j = -1
-        # for key, value in new.items():
-        #     j = j + 1
-        #     a = value
-        #     new.popitem()
-        #     for k, key in enumerate(new):
-        #         if key == a:
-        #             return(j, k)
-        #             break
-                
