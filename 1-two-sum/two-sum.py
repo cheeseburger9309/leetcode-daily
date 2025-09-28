52226eb4582d351e714161781ea20ce3dc4dfb5e
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2025-09-27 20:14:06 -0500

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.17%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,12 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # hashmap = {}
+
+        # for i, n in enumerate(nums):
+        #     diff = target - n
+        #     if diff in hashmap:
+        #         return [hashmap[diff], i]
+        #     hashmap[n] = i
+        
         hashmap = {}
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in hashmap:
-                return [hashmap[diff], i]
-            hashmap[n] = i
-
-        
+        for i in range(len(nums)):
+            if (target - nums[i]) in hashmap:
+                return hashmap[target-nums[i]], i
+            else:
+                hashmap[nums[i]] = i
