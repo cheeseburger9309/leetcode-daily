d47b3c1ef37f077cf5eec4bc29fc00d20871f181
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2025-09-27 20:20:09 -0500

    Time: 23 ms (55.59%) | Memory: 13.5 MB (41.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,20 +5,6 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-
-        hashmap_s, hashmap_t = {}, {}
-
-        for i in range(len(s)):
-            hashmap_s[s[i]] = 1 + hashmap_s.get(s[i], 0)
-        
-        for j in range(len(t)):
-            hashmap_t[t[j]] = 1 + hashmap_t.get(t[j], 0)
-
-        if hashmap_t == hashmap_s:
-            return True
-        else:
-            return False
-        
+        return sorted(s) == sorted(t)
+    
         
