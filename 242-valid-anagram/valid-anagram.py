11e7508be18241f5f7d300dc825707893e95873e
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2026-01-05 16:53:51 -0600

    Time: 27 ms (24.41%) | Memory: 13 MB (47.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,20 +5,8 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        
-        hashmap = {}
+        sorted_s = ''.join(sorted(s))
+        sorted_t = ''.join(sorted(t))
 
-        for char in s:
-            hashmap[char] = 1 + hashmap.get(char, 0)
-        
-        for char in t:
-            if char not in hashmap or hashmap[char] == 0:
-                return False
-            hashmap[char] -= 1 
-        return True
-
-        
-    
-        
+        return sorted_s == sorted_t
+            
