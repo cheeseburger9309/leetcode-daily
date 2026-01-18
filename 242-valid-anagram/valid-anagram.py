5fc596308b5b479ea5bca0b3bcac9a4c5fb50644
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2026-01-18 14:08:35 -0600

    Time: 11 ms (95.42%) | Memory: 12.6 MB (68.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,12 +5,22 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        
-        str1 = sorted(s)
-        str2 = sorted(t)
+        str1 = {}
+        str2 = {}
 
+        for char in s:
+            if char in str1:
+                str1[char] += 1
+            else:
+                str1[char] = 1
+
+        for char in t:
+            if char in str2:
+                str2[char] += 1
+            else:
+                str2[char] = 1
+        
         return str1 == str2
+        
 
             
