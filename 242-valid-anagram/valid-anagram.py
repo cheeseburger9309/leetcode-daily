654a74dfadb7d76bf9399f69ea8cf7ee92c575dd
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2026-01-18 14:14:29 -0600

    Time: 10 ms (96.09%) | Memory: 12.5 MB (82.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(s) != len(t):
+            return False
+
         str1 = {}
-        str2 = {}
 
         for char in s:
             if char in str1:
@@ -15,12 +17,12 @@ class Solution(object):
                 str1[char] = 1
 
         for char in t:
-            if char in str2:
-                str2[char] += 1
+            if char in str1:
+                str1[char] -= 1
             else:
-                str2[char] = 1
+                str1[char] = 1
         
-        return str1 == str2
+        return not any(str1.values()) 
         
 
             
