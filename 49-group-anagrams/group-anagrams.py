cb2319acd708a47d70508833b5ac3fe4796b7a28
Sai Kasyap Jannabhatla <jv.saikasyap@gmail.com>
2026-01-18 14:46:53 -0600

    Time: 12 ms (98.30%) | Memory: 16.6 MB (47.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        hashmap = defaultdict(list)
+
+        hashmap = {}
+
         for s in strs:
-            charArray = [0] * 26
-            for c in s:
-                charArray[ord(c) - ord("a")] += 1
-            hashmap[tuple(charArray)].append(s)
+            key = tuple(sorted(s))
+            if key in hashmap:
+                hashmap[key].append(s)
+            else:
+                hashmap[key] = [s]
+        
         return hashmap.values()
-            
-        
-        
         
